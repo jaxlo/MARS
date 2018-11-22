66f43cdee013fba56710b347821d2207bf8f4de6
TJ Reynolds <reynolds6404@gmail.com>
2018-11-21 19:38:43 -0700

    Update groundControl.py

--- groundControl.py
+++ groundControl.py
@@ -1,5 +1,5 @@
 #Made by Jackson and TJ in 2018
-#Takes GPS out put and formats it into a more readable format
+#Takes GPS output and formats it into a more readable format
 import arrow
 gps_input = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"
 #would start blank
