e5f78db47c03b31b3477cc751d82bb53fd095c8b
TJ Reynolds <reynolds6404@gmail.com>
2018-11-22 09:43:01 -0700

    Update rpicam.py
    NOT DONE

--- rpicam.py
+++ rpicam.py
@@ -1,18 +1,46 @@
 #By Jackson Lohman and TJ Reynolds in 2018
+#camera program for Deimos
+from time import *
+from picamera import picamera
+start_time = time.time()
+time_save = 1500 #25min
+
+start = 0 #set to 1 to meet conditions below
+stop = 0 #set to 1 to meet conditions below
 
 try:
-	from picamera import PiCamera
 	picam = PiCamera()
-	picam.resolution = (320,320)#run now to give it time to load, change reslution
+	picam.resolution = (1920,1080)#change to (1280,720) for Phobos
+	picam.framerate = 30
+	picam.start_preview()#comment out if not connected to an external monitor
 	camSupport = True
-	print('Pi Camera Enabled')
+	print('picam enabled')
+	time.sleep(5)
 except ImportError:
 	camSupport = False
-	print('Pi Camera Disabled')
-  
+	print('picam disabled')
+
+def record():
+	picam_start = True
+	while picam_start == True:
+		start = #new input from socket (1) --- probably call socketListen() here
+		if start == 1:
+			picam.start_recording('/home/MARS/deimoscam.h264')
+			picam_start = False
+		else:
+			pass
+
+	picam_stop = True
+	while picam_stop == True:
+		stop = #new input from socket (1) --- probably call socketListen() here
+		if stop == 1 or time.time() > start_time + time_save:
+			picam.stop_recording()
+			picam.stop_preview()#comment out if not connected to an external monitor
+			picam_stop = False
+		else:
+			pass
+
 def socketListen():
-  pass
+	pass
 def socketSend():
- pass
-def Record():
-  pass
+	pass
