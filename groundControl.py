184c1482f7f77339385b6b38692e66667454e4be
TJ Reynolds <reynolds6404@gmail.com>
2018-11-23 07:02:48 -0700

    Update groundControl.py
    NOT COMPLETE! still need to change it so it uses new data from the GPS everytime it runs the LoRa function

--- groundControl.py
+++ groundControl.py
@@ -1,45 +1,90 @@
-#Made by Jackson Lohman and TJ Reynolds in 2018
-#Takes GPS output and formats it into a more readable format
+#Made by Jackson Lohman and TJ Reynolds 2018
+#runs on the ground
 import arrow
-gps_input = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"
-#would start blank
+import socket
+import time
+
+#----- GLOBAL VARS -----#
+function = int(input('Which would you like to communicate with: \n 0: Phobos and Deimos \n 1:LoRa'))
+gps_input = ''
 f = open("C:\\Users\\reyno\\Documents\\marsgps.txt", "a") #change to whatever the .txt is
+NetworkPort = 59281
+command = 1
+#-----------------------#
 
-def format_data():
-	date = arrow.now().format('MM/DD/YYYY')
-	time = gps_input[7:13]
-	hour = time[:2]
-	az_hour = str(int(hour) - 7)#comment out this line for UTC, or change number for other timezones
-	minute = time[2:4]
-	second = time[4:]
-	final_time = 'Time: '+ az_hour+':'+minute+':'+second+' '+date +' MST'
-	print('\n'+str(final_time))
-	f.write('\n\n'+str(final_time))
+def selection():
+	if function == 0:
+		rpi.run_socket()
+	elif function == 1:
+		lora.run_format()
+	else:
+		print('Invalid Input, Try Again')
+		selection()
 
-	lat = gps_input[14:24]
-	deg_lat = lat[:2]
-	sec_lat = lat[2:8]
-	dir_lat = lat[9:]
-	final_lat = 'Latitude: '+deg_lat+'° '+sec_lat+"' " +dir_lat
-	print(str(final_lat))
-	f.write('\n'+str(final_lat))
+class lora(): #NEED to still to make it so that it brings in new gps data
+	def format_data():
+		date = arrow.now().format('MM/DD/YYYY')
+		time = gps_input[7:13]
+		hour = time[:2]
+		az_hour = str(int(hour) - 7)
+		minute = time[2:4]
+		second = time[4:]
+		final_time = 'Time: '+ az_hour+':'+minute+':'+second+' '+date +' MST'
+		print('\n'+str(final_time))
+		f.write('\n\n'+str(final_time))
 
-	long = gps_input[25:36]
-	deg_long = long[:3]
-	sec_long = long[3:9]
-	dir_long = long[10:]
-	final_long = 'Longitude: '+deg_long+'° '+sec_long+"' " +dir_long
-	print(str(final_long))
-	f.write('\n'+str(final_long))
+		lat = gps_input[14:24]
+		deg_lat = lat[:2]
+		sec_lat = lat[2:8]
+		dir_lat = lat[9:]
+		final_lat = 'Latitude: '+deg_lat+'° '+sec_lat+"' " +dir_lat
+		print(str(final_lat))
+		f.write('\n'+str(final_lat))
 
-	alt = gps_input[46:53]
-	length = alt[0:5]
-	final_alt = 'Altitude: '+length+' meters'
-	print(str(final_alt))
-	f.write('\n'+str(final_alt))
+		long = gps_input[25:36]
+		deg_long = long[:3]
+		sec_long = long[3:9]
+		dir_long = long[10:]
+		final_long = 'Longitude: '+deg_long+'° '+sec_long+"' " +dir_long
+		print(str(final_long))
+		f.write('\n'+str(final_long))
 
+		alt = gps_input[46:53]
+		length = alt[0:5]
+		final_alt = 'Altitude: '+length+' meters'
+		print(str(final_alt))
+		f.write('\n'+str(final_alt))
 
-while True:
-	format_data()
-	gps_input = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47" 
-	#would be new reading
+	def run_format():
+		while True:
+			gps_input = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"  #need to change to get a new reading (probably function call)
+			lora.format_data()
+			
+class rpi(): #turns on and off camera
+	def socketSend():
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.bind(('', NetworkPort))#accept any ip address
+		sock.listen(1)
+		print('Listening for Connection...')
+		conn, addr = sock.accept()
+		print('connected to' + str(addr))
+		
+		conn.sendall(str(command).encode())
+		print('command sent')
+		sock.close()
+
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.bind(('', NetworkPort))#accept any ip address
+		sock.listen(1)
+		print('Listening for Connection...')
+		conn, addr = sock.accept()
+		print('connected to' + str(addr))
+		
+		conn.sendall(str(command).encode())
+		print('command sent')
+		sock.close()
+
+	def run_socket():
+		rpi.socketSend()
+
+selection()
