a2a7a7e2bee7c165f72adf38f637cfe15bddd7e8
TJ Reynolds <reynolds6404@gmail.com>
2018-12-08 16:15:00 -0700

    Update gpsOutput.ino

--- gpsOutput.ino
+++ gpsOutput.ino
@@ -1,27 +1,17 @@
 // Made by Jackson Lohman and TJ Reynolds 2018
 // Part of this code is borrowed from adafruit.com
+
 #define GPSSerial Serial1
  
  
 void setup() {
-  // wait for hardware serial to appear
-  while (!Serial);
- 
-  // make this baud rate fast enough to we aren't waiting on it
-  Serial.begin(115200);
- 
-  // 9600 baud is the default rate for the Ultimate GPS
+  // 9600 baud is the default rate for the GPS
   GPSSerial.begin(9600);
 }
  
-     
-void loop() {
-  if (Serial.available()) {
-    char c = Serial.read();
-    GPSSerial.write(c);
-  }
+void loop() {  
   if (GPSSerial.available()) {
     char c = GPSSerial.read();
     Serial.write(c);
-  }
+    }
 }
