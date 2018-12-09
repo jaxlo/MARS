f8925c48f38d11b4dc746fc53e7228f0b6874abd
TJ Reynolds <reynolds6404@gmail.com>
2018-12-08 20:18:52 -0700

    Delete pyreadserial.py

--- pyreadserial.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-ser = serial.Serial('/dev/ttyACMO', 9600)
-ser.baudrate = 9600
-
-while True:
-  read_ser = ser.readline()
-  print(read_ser) #need to get rid of b' and '\r\n on each output line
