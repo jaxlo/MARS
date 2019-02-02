898413b8a3bc19f6aefdfd2bfedc00e4774dd589
TJ Reynolds <reynolds6404@gmail.com>
2019-02-01 21:35:29 -0700

    Update loraSend.ino

--- loraSend.ino
+++ loraSend.ino
@@ -1,85 +1,70 @@
 // Made by Jackson Lohman and TJ Reynolds 2018
-// Part of this code is borrowed from adafruit.com
+// Part of this code is borrowed from adafruit.com and https://github.com/robertsmarty/adafruit3078gps/blob/master/Adafruit%20Feather%2032u4%20LoRa/GPS_TX.ino#L80
 
 #include <SPI.h>
 #include <RH_RF95.h>
-
-//for feather m0  
+ 
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
 
 
-// Change to 434.0 or other frequency, must match RX's freq!
-#define RF95_FREQ 434.0
-
+// TX must match RX's freq!
+#define RF95_FREQ 915.0
+ 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
-
-#define GPSSerial Serial1
-
+ 
+// Blinky on receipt
+#define LED 13
+ 
 void setup() 
 {
+// Configure onboard LED
+  pinMode(LED, OUTPUT); 
 
-  GPSSerial.begin(9600);
-  
+// Configure reset pin for output
   pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
 
-  Serial.begin(115200);
- 
- 
 
-  // manual reset
+// Configure GPS serial port
+  Serial1.begin(9600);
+
+// Configure serial port for debugging
+  Serial.begin(9600);
+  delay(100);
+ 
+// Reset the radio
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
 
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
-  Serial.println("LoRa radio init OK!");
+// Initialize radio 
+  rf95.init();
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 5 to 23 dBm:
+// Set frequency, power and slow-long range
+  rf95.setFrequency(RF95_FREQ);
   rf95.setTxPower(23, false);
 }
-
-int16_t packetnum = 0;  // packet counter, we increment per xmission
-int val = 0;
-char line[74];
-char c;
-
+ 
 void loop()
 {
-  while(c != '$'){
-    c = GPSSerial.read();
-    Serial.write(c); // remove write line (or comment out) so that it doesnt try to write to serial before sending over lora
-    line[val] = c;
-    val++;
-    delay(250); // Wait 1/4 second between transmits, could also 'sleep' here!
-  }
-  val = 0;
-  //line = {}; // need to remove all items from array
-  
-  rf95.send((uint8_t *)line, 74);
+  char radiodata[1000],c=0;
+  String gpsdata,year;
 
-  //Serial.println("\nWaiting for packet to complete..."); 
+// Read data from GPS module until end of line  
+  c=0;
+  while(c !=10) {
+    if(Serial1.available()) {
+      c=Serial1.read();
+      gpsdata+=c;
+      //Serial.print(c);
+    }
+  }
+  Serial.print(gpsdata);
+  gpsdata.toCharArray(radiodata, gpsdata.length()+1);
+  rf95.send((uint8_t *)radiodata, gpsdata.length()+1);
   rf95.waitPacketSent();
-  
-  
-  
 
 }
